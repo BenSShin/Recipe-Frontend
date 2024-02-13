0ebf4432fdc392d996d60b2adf3df697c0c1ce63
ben <ben.shin27@gmail.com>
2024-02-13 11:09:09 -0600

    added recipe index

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,11 +1,23 @@
 import { Home } from "../Home/Home";
 import { RecipeIndex } from "../Recipe/RecipeIndex";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export function Content() {
+  const [recipes, setRecipes] = useState([]);
+
+  const handleIndexRecipes = () => {
+    axios.get("http://localhost:3000/recipes.json").then((response) => {
+      setRecipes(response.data);
+    });
+  };
+
+  useEffect(handleIndexRecipes, []);
+
   return (
     <>
       <Home />
-      <RecipeIndex />
+      <RecipeIndex recipes={recipes} />
     </>
   );
 }
--- src/Recipe/RecipeIndex.jsx
+++ src/Recipe/RecipeIndex.jsx
@@ -1,22 +1,10 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
-export function RecipeIndex() {
-  const [recipes, setRecipes] = useState([]);
-
-  const handleIndexRecipes = () => {
-    axios.get("http://localhost:3000/recipes.json").then((response) => {
-      setRecipes(response.data);
-    });
-  };
-
-  useEffect(handleIndexRecipes, []);
-
+/* eslint-disable react/prop-types */
+export function RecipeIndex(props) {
   return (
     <div>
       <h1>All Recipes</h1>
       <div>
-        {recipes.map((recipe) => (
+        {props.recipes.map((recipe) => (
           <div key={recipe.id}>
             <h2>{recipe.name}</h2>
             <p>Prep Time: {recipe.friendly_prep_time}</p>
